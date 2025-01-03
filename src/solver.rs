75b4e58fd317513efff46b8c5b9c9ca52f565a45
Kaushik Varadharajan <me@kaushikv.com>
2025-01-03 01:05:16 -0800

    feat: improve how assignments are stored

--- src/solver.rs
+++ src/solver.rs
@@ -1,30 +1,19 @@
-use super::formula::{Var, CNF, Assignment};
-use std::collections::HashMap;
+use super::formula::{Assignment, CNF};
 
 use itertools::Itertools;
 
 pub fn solve_basic(cnf: &CNF) -> Option<Assignment> {
     // Literally iterate through every possible assignment.
-
-    let mut count = 0;
-
-    let res = std::iter::repeat([false, true])
+    std::iter::repeat([false, true])
         .take(cnf.num_vars as usize)
         .multi_cartesian_product()
+        .map(|v| Assignment::from_vector(v.to_vec()))
         .find(|assignment| {
-            count += 1;
-            cnf.clauses
-                .iter()
-                .all(|clause| clause.literals.iter().any(|lit| assignment[lit.var.index as usize - 1] == lit.positive))
-        });
-    
-    println!("Checked {} assignments", count);
-    
-    res.map(|assignment| {
-        let mut res = HashMap::new();
-        for (var, value) in assignment.iter().enumerate() {
-            res.insert(Var { index: (var + 1) as u32 }, *value);
-        }
-        Assignment { assignment: res }
-    })
+            cnf.clauses.iter().all(|clause| {
+                clause
+                    .literals
+                    .iter()
+                    .any(|lit| assignment.get(&lit.var) == lit.positive)
+            })
+        })
 }
