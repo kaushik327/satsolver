9737a229f58455ff64468f056b7e9eb9e4b892af
Kaushik Varadharajan <kaushik.r.varadharajan@gmail.com>
2025-01-03 21:04:00 -0800

    feat: basic recursive solver

--- src/formula.rs
+++ src/formula.rs
@@ -21,19 +21,37 @@ pub struct CNF {
     pub clauses: Vec<Clause>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Assignment {
-    assignment: Vec<bool>,
+    assignment: Vec<Option<bool>>,
 }
 
 impl Assignment {
-    pub fn from_vector(assignment: Vec<bool>) -> Self {
+    pub fn from_vector(assignment: Vec<Option<bool>>) -> Self {
         Self { assignment }
     }
-    pub fn get(&self, var: &Var) -> bool {
+    pub fn get(&self, var: &Var) -> Option<bool> {
         self.assignment[var.index as usize - 1]
     }
-    pub fn set(&mut self, var: &Var, value: bool) {
-        self.assignment[var.index as usize - 1] = value;
+    pub fn set(&self, var: &Var, value: bool) -> Self {
+        let mut new_assignment = self.assignment.clone();
+        new_assignment[var.index as usize - 1] = Some(value);
+        Self::from_vector(new_assignment)
+    }
+    pub fn get_unassigned_var(&self) -> Option<Var> {
+        self.assignment
+            .iter()
+            .position(|v| v.is_none())
+            .map(|n| Var {
+                index: (n + 1) as u32,
+            })
+    }
+    pub fn fill_unassigned(&self) -> Self {
+        let new_assignment = self
+            .assignment
+            .iter()
+            .map(|v| v.or(Some(false)))
+            .collect::<Vec<_>>();
+        Self::from_vector(new_assignment)
     }
 }
--- src/solver.rs
+++ src/solver.rs
@@ -2,18 +2,52 @@ use super::formula::{Assignment, CNF};
 
 use itertools::Itertools;
 
+// pub fn unit_propagate(cnf: &CNF, assignment: &Assignment) -> (CNF, Assignment) {
+//     todo!()
+// }
+
+pub fn check_assignment(cnf: &CNF, assignment: &Assignment) -> Option<bool> {
+    // Evaluates incomplete assignment on CNF formula and determines if
+    // the formula is satisfied, falsified, or neither yet.
+
+    for clause in &cnf.clauses {
+        let bools = clause
+            .literals
+            .iter()
+            .map(|lit| assignment.get(&lit.var).map(|b| b == lit.positive))
+            .collect::<Vec<_>>();
+
+        if bools.iter().all(|v| v == &Some(false)) {
+            return Some(false);
+        }
+        if !bools.iter().any(|v| v == &Some(true)) {
+            return None;
+        }
+    }
+    return Some(true);
+}
+
 pub fn solve_basic(cnf: &CNF) -> Option<Assignment> {
     // Literally iterate through every possible assignment.
-    std::iter::repeat([false, true])
+    std::iter::repeat([Some(false), Some(true)])
         .take(cnf.num_vars as usize)
         .multi_cartesian_product()
         .map(|v| Assignment::from_vector(v.to_vec()))
-        .find(|assignment| {
-            cnf.clauses.iter().all(|clause| {
-                clause
-                    .literals
-                    .iter()
-                    .any(|lit| assignment.get(&lit.var) == lit.positive)
-            })
-        })
+        .find(|assignment| check_assignment(cnf, assignment).is_some_and(|x| x))
+}
+
+pub fn solve_backtrack(cnf: &CNF) -> Option<Assignment> {
+    // Recursively assign each variable to true or false
+    pub fn solve_backtrack_rec(cnf: &CNF, cube: &Assignment) -> Option<Assignment> {
+        match check_assignment(cnf, &cube) {
+            Some(true) => Some(cube.fill_unassigned()),
+            Some(false) => None,
+            None => cube.get_unassigned_var().and_then(|v| {
+                solve_backtrack_rec(cnf, &cube.set(&v, false))
+                    .or(solve_backtrack_rec(cnf, &cube.set(&v, true)))
+            }),
+        }
+    }
+    let blank_assignment = &Assignment::from_vector(vec![None; cnf.num_vars as usize]);
+    solve_backtrack_rec(cnf, blank_assignment)
 }
