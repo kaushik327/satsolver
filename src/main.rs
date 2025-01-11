4e949ab705797615db8e7f6f3cd0da0a137599a2
Kaushik Varadharajan <me@kaushikv.com>
2025-01-10 23:49:36 -0800

    add example cnf formulas and timing

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,34 @@ mod solve_simple;
 mod solver_state;
 
 use std::io;
+use std::time::Instant;
+
+type SolverFn = fn(&formula::CnfFormula) -> Option<formula::Assignment>;
 
 fn main() {
     let mut reader = io::BufReader::new(io::stdin());
     let cnf = parser::parse_dimacs(&mut reader).unwrap();
-    println!("{:#?}", cnf);
-    println!("{:#?}", solve_simple::solve_basic(&cnf));
-    println!("{:#?}", solve_simple::solve_backtrack(&cnf));
-    println!("{:#?}", solve_simple::solve_dpll(&cnf));
-    println!("{:#?}", solve_cdcl::solve_cdcl(&cnf));
+    println!("{:?}", cnf);
+
+    let solvers: &[(&str, SolverFn)] = &[
+        ("CDCL", solve_cdcl::solve_cdcl),
+        ("DPLL", solve_simple::solve_dpll),
+        ("Backtracking", solve_simple::solve_backtrack),
+        ("Basic", solve_simple::solve_basic),
+    ];
+
+    for (solver_name, solver) in solvers {
+        println!("Solver: {}", solver_name);
+        let start_time = Instant::now();
+        let answer = solver(&cnf);
+        let duration = start_time.elapsed();
+        println!("Solver runtime: {:?}", duration);
+
+        // We don't have proofs of unsatisfiability yet.
+        assert!(
+            answer.is_none()
+                || answer.is_some_and(|a| a.get_unassigned_var().is_none()
+                    && solver_state::check_assignment(&cnf, &a))
+        );
+    }
 }
