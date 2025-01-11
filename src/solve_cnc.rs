d4c0f6cfb1475b8f5228b8bb3acf91da5253085f
Kaushik Varadharajan <kaushik.r.varadharajan@gmail.com>
2025-01-11 00:50:10 -0800

    feat(scuffed): multi-threaded cnc

--- src/solve_cnc.rs
+++ src/solve_cnc.rs
@@ -1,20 +1,22 @@
 use crate::formula::*;
 use crate::solve_cdcl::*;
 use crate::solver_state::*;
+use std::sync::mpsc;
+use std::thread;
 
 pub fn solve_cnc(cnf: &CnfFormula, depth: usize) -> Option<Assignment> {
-
-    pub fn solve_cnc_rec(state: &CdclState, depth: usize) -> Option<Assignment> {
+    pub fn solve_cnc_rec(state: &CdclState, depth: usize, tx: mpsc::Sender<Option<Assignment>>) {
         let mut ucp_state = state.clone();
 
         while let Some((trail_elem, ucp_result)) = unit_propagate(&ucp_state.state) {
             ucp_state.state = ucp_result;
             ucp_state.trail.push(trail_elem);
         }
+
         if ucp_state.state.is_satisfied() {
-            Some(ucp_state.state.assignment.fill_unassigned())
+            let _ = tx.send(Some(ucp_state.state.assignment.fill_unassigned()));
         } else if ucp_state.state.is_falsified() {
-            None
+            let _ = tx.send(None);
         } else if depth > 0 {
             let unassigned_var = ucp_state.state.assignment.get_unassigned_var().unwrap();
 
@@ -50,9 +52,15 @@ pub fn solve_cnc(cnf: &CnfFormula, depth: usize) -> Option<Assignment> {
                 state: snapshot.assign(&unassigned_var.clone(), Val::False),
             };
 
-            solve_cnc_rec(&true_state, depth - 1).or(solve_cnc_rec(&false_state, depth - 1))
+            let tx1 = tx.clone();
+            let tx2 = tx.clone();
+
+            thread::spawn(move || solve_cnc_rec(&true_state, depth - 1, tx1));
+            thread::spawn(move || solve_cnc_rec(&false_state, depth - 1, tx2));
         } else {
-            solve_cdcl_from_cdcl_state(&mut ucp_state)
+            thread::spawn(move || {
+                let _ = tx.send(solve_cdcl_from_cdcl_state(&mut ucp_state));
+            });
         }
     }
 
@@ -64,5 +72,27 @@ pub fn solve_cnc(cnf: &CnfFormula, depth: usize) -> Option<Assignment> {
         trail: vec![],
     };
 
-    solve_cnc_rec(&state_with_trail, depth)
+    let (tx, rx) = mpsc::channel();
+    solve_cnc_rec(&state_with_trail, depth, tx);
+
+    rx.recv().into_iter().find(|x| x.is_some()).flatten()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parser::parse_dimacs_str;
+
+    #[test]
+    fn test_solve_cnc_sat() {
+        let cnf = parse_dimacs_str(b"\np cnf 5 4\n1 2 0\n1 -2 0\n3 4 0\n3 -4 0").unwrap();
+        assert!(solve_cnc(&cnf, 3)
+            .is_some_and(|a| a.get_unassigned_var().is_none() && check_assignment(&cnf, &a)));
+    }
+
+    #[test]
+    fn test_solve_cnc_unsat() {
+        let cnf = parse_dimacs_str(b"\np cnf 5 5\n1 2 0\n1 -2 0\n3 4 0\n3 -4 0\n-1 -3 0").unwrap();
+        assert!(solve_cnc(&cnf, 3).is_none());
+    }
 }
